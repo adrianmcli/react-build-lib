485e065bcb6fe1ddc4044cb1906041be26de1835
Adrian Li <li.adrianmc@gmail.com>
2016-12-29 06:31:43 -0500

    allow stage 0

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,24 @@
 #!/usr/bin/env node
 
+const program = require('commander');
 const rimrafSync = require('rimraf').sync;
 const exec = require('child_process').exec;
 
-const INPUT_DIR = process.argv[2] || 'src';
-const OUTPUT_DIR = process.argv[3] || 'lib';
+// get inputs
+program
+  .version('0.0.1')
+  .option('-b, --stage-0', 'Turn on stage-0 compilation.')
+  .parse(process.argv);
 
-rimrafSync(OUTPUT_DIR);
+const inputDir = program.args[0] || 'src';
+const outputDir = program.args[1] || 'lib';
+
+rimrafSync(outputDir);
 
 const babelExec = 'node_modules/babel-cli/bin/babel.js';
-const presets = 'react,latest,stage-3';
+const presets = program.stage0 ? 'react,latest,stage-0' : 'react,latest,stage-3';
 
-const cmd = `${babelExec} ${INPUT_DIR} --out-dir ${OUTPUT_DIR} --presets ${presets} --copy-files`;
+const cmd = `${babelExec} ${inputDir} --out-dir ${outputDir} --presets ${presets} --copy-files`;
 
 exec(cmd, function(error, stdout, stderr) {
   if (error) {
